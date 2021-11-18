75d69ecc256754c3c7e15b8dae6e6031dff20069
i'm kamil <kamil@25wat.com>
2021-11-18 18:39:56 +0100

    fix buttons

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-c9a29d4f8725430f5085.js"
+    "url": "webpack-runtime-73d1999637bc337e232c.js"
   },
   {
     "url": "framework-8e528b732ab2eaadb7b7.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "d8fa7d4da0e8924db9c138639863d5bf"
+    "revision": "b8fa4fc0279baaa6ad4eed5942a06ce7"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-b0556ce5127c1a3e2490.js"
